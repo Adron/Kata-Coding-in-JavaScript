0e297ff6c49a466762a2ebe3da67c950f445aa32
Adron Hall <adronhall@gmail.com>
2011-09-23 16:55:24 -0700

    First commit, no tests, ready to start the kata.  i.e. Start here if you want to do this yourself without anything implemented. :)

---
+++ Scripts/NumeralConversion.js
@@ -0,0 +1,12 @@
+/**
+ * User: Adron B. Hall
+ * Date: 9/23/11
+ * Time: 4:20 PM <- Yup, that's the time I started it at.
+ * Description: This library is created for converting Arabic to Roman and
+ * Roman to Arabic Numerals. For usage, check out the blog entry...
+ *
+ *
+ * For code, notes, and other errata check out the Github Repo...
+ *
+ * 
+ */
